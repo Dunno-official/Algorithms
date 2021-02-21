f98a7b7658b20399d4b11c597dccaf48bc688bf9
Denis Bondar <60221683+Dunno-official@users.noreply.github.com>
2021-02-21 23:54:19 +0200

    .

--- Dynamic programming/17 Gardener/gardener.cpp
+++ Dynamic programming/17 Gardener/gardener.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
-
+#include <cmath>
 using namespace std;
 
 int main()
 {
+	long long n = 0;
+	cin >> n;
 
+	long long num = powl(2 , (n - 1));
+
+	if (num == 0)
+		num = 1;
+
+	cout << 3 * num;
 }
